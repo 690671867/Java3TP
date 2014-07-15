3547d49ac4c10f53859d5bc0f78c89c1286828b4
candous <candido@mobile-midia.com>
2014-07-15 13:26:13 -0400

    Mise a jour Raphael

---
+++ Raphael/pkgBackingBeans/RechercheBackingBean.java
@@ -0,0 +1,324 @@
+package pkgBackingBeans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.faces.model.SelectItem;
+
+import pkgEntities.Categorie;
+import pkgEntities.City;
+import pkgEntities.EyeColor;
+import pkgEntities.HairColor;
+import pkgEntities.Hobby;
+import pkgEntities.Membre;
+import pkgEntities.NiveauMembre;
+import pkgEntities.SkinColor;
+import pkgEntities.Status;
+import pkgEntities.WeightRange;
+import pkgManagers.MembreManager;
+import pkgServices.SendEmail;
+
+public class RechercheBackingBean implements Serializable {
+	
+	private MembreBackingBean mbb;
+	
+	//racolte des infos
+	private Integer categorie_id;
+	private ArrayList<Integer> hairColor_id;
+	private ArrayList<Integer> skinColor_id;
+	private ArrayList<Integer> eyeColor_id;
+	private Integer typePhysique_id;
+	private Integer ville_id;
+	private ArrayList<Integer> status_id;
+	private ArrayList<Integer> hobbies_id;
+	private Integer tailleMax;
+	private Integer tailleMin;
+	private Boolean fumeur;
+	
+	
+	//affichages des options
+	private ArrayList<SelectItem> listeCategorie;
+	private ArrayList<SelectItem> listeHairColor;
+	private ArrayList<SelectItem> listeSkinColor;
+	private ArrayList<SelectItem> listeEyeColor;
+	private ArrayList<SelectItem> listeTypePhysique;
+	private ArrayList<SelectItem> listeVilles;
+	private ArrayList<SelectItem> listeStatus;
+	private ArrayList<SelectItem> listeHobbies;
+	
+	
+	//liste de membres pour afficher les resultats
+	private ArrayList<Membre> listeResultat;
+	
+	
+	
+	public MembreBackingBean getMbb() {
+		return mbb;
+	}
+	public void setMbb(MembreBackingBean mbb) {
+		this.mbb = mbb;
+	}
+	
+	
+	public Integer getCategorie_id() {
+		return categorie_id;
+	}
+	public void setCategorie_id(Integer categorie_id) {
+		this.categorie_id = categorie_id;
+	}
+	public ArrayList<Integer> getHairColor_id() {
+		return hairColor_id;
+	}
+	public void setHairColor_id(ArrayList<Integer> hairColor_id) {
+		this.hairColor_id = hairColor_id;
+	}
+	public ArrayList<Integer> getSkinColor_id() {
+		return skinColor_id;
+	}
+	public void setSkinColor_id(ArrayList<Integer> skinColor_id) {
+		this.skinColor_id = skinColor_id;
+	}
+	public ArrayList<Integer> getEyeColor_id() {
+		return eyeColor_id;
+	}
+	public void setEyeColor_id(ArrayList<Integer> eyeColor_id) {
+		this.eyeColor_id = eyeColor_id;
+	}
+	public Integer getTypePhysique_id() {
+		return typePhysique_id;
+	}
+	public void setTypePhysique_id(Integer typePhysique_id) {
+		this.typePhysique_id = typePhysique_id;
+	}
+	public Integer getVille_id() {
+		return ville_id;
+	}
+	public void setVille_id(Integer ville_id) {
+		this.ville_id = ville_id;
+	}
+	public ArrayList<Integer> getStatus_id() {
+		return status_id;
+	}
+	public void setStatus_id(ArrayList<Integer> status_id) {
+		this.status_id = status_id;
+	}
+	public ArrayList<Integer> getHobbies_id() {
+		return hobbies_id;
+	}
+	public void setHobbies_id(ArrayList<Integer> hobbies_id) {
+		this.hobbies_id = hobbies_id;
+	}
+	public Integer getTailleMax() {
+		return tailleMax;
+	}
+	public void setTailleMax(Integer tailleMax) {
+		this.tailleMax = tailleMax;
+	}
+	public Integer getTailleMin() {
+		return tailleMin;
+	}
+	public void setTailleMin(Integer tailleMin) {
+		this.tailleMin = tailleMin;
+	}
+	public Boolean getFumeur() {
+		return fumeur;
+	}
+	public void setFumeur(Boolean fumeur) {
+		this.fumeur = fumeur;
+	}
+	public ArrayList<Membre> getListeResultat() {
+		return listeResultat;
+	}
+	public void setListeResultat(ArrayList<Membre> listeResultat) {
+		this.listeResultat = listeResultat;
+	}
+	public ArrayList<SelectItem> getListeCategorie() {
+
+		listeCategorie = new ArrayList<SelectItem>();
+		ArrayList<Categorie> categories = new ArrayList<Categorie>();
+		categories = CategorieManager.getListeCategories();
+		for (Categorie c : categories) {
+			SelectItem item = new SelectItem(c.getCategorieID(),
+					c.getDescription());
+			listeCategorie.add(item);
+		}
+
+		return listeCategorie;
+	}
+
+	public void setListeCategorie(ArrayList<SelectItem> listeCategorie) {
+		this.listeCategorie = listeCategorie;
+	}
+
+	public ArrayList<SelectItem> getListeHairColor() {
+
+		listeHairColor = new ArrayList<SelectItem>();
+		ArrayList<HairColor> hairColors = new ArrayList<HairColor>();
+		hairColors = HairColorManager.getListeHairColors();
+		for (HairColor hc : hairColors) {
+			SelectItem item = new SelectItem(hc.getId(), hc.getDescription());
+			listeHairColor.add(item);
+		}
+
+		return listeHairColor;
+	}
+
+	public void setListeHairColor(ArrayList<SelectItem> listeHairColor) {
+		this.listeHairColor = listeHairColor;
+	}
+
+	public ArrayList<SelectItem> getListeSkinColor() {
+
+		listeSkinColor = new ArrayList<SelectItem>();
+		ArrayList<SkinColor> skinColors = new ArrayList<SkinColor>();
+		skinColors = SkinColorManager.getListeSkinColors();
+		for (SkinColor sc : skinColors) {
+			SelectItem item = new SelectItem(sc.getId(), sc.getDescription());
+			listeSkinColor.add(item);
+		}
+		return listeSkinColor;
+	}
+
+	public void setListeSkinColor(ArrayList<SelectItem> listeSkinColor) {
+		this.listeSkinColor = listeSkinColor;
+	}
+
+	public ArrayList<SelectItem> getListeEyeColor() {
+
+		listeEyeColor = new ArrayList<SelectItem>();
+		ArrayList<EyeColor> eyeColors = new ArrayList<EyeColor>();
+		eyeColors = EyeColorManager.getListeEyeColors();
+		for (EyeColor ec : eyeColors) {
+			SelectItem item = new SelectItem(ec.getId(), ec.getDescription());
+			listeEyeColor.add(item);
+		}
+		return listeEyeColor;
+	}
+
+	public void setListeEyeColor(ArrayList<SelectItem> listeEyeColor) {
+		this.listeEyeColor = listeEyeColor;
+	}
+
+	public ArrayList<SelectItem> getListeTypePhysique() {
+
+		listeTypePhysique = new ArrayList<SelectItem>();
+		ArrayList<WeightRange> typesPhysiques = new ArrayList<WeightRange>();
+		typesPhysiques = WeightRangeManager.getListeWeightRanges();
+		for (WeightRange wr : typesPhysiques) {
+			SelectItem item = new SelectItem(wr.getId(), wr.getDescription());
+			listeTypePhysique.add(item);
+		}
+		return listeTypePhysique;
+	}
+
+	public void setListeTypePhysique(ArrayList<SelectItem> listeTypePhysique) {
+		this.listeTypePhysique = listeTypePhysique;
+	}
+
+	public ArrayList<SelectItem> getListeStatus() {
+		listeStatus = new ArrayList<SelectItem>();
+		ArrayList<Status> lesStatus = new ArrayList<Status>();
+		lesStatus = StatusManager.getListeStatus();
+		for (Status st : lesStatus) {
+			SelectItem item = new SelectItem(st.getId(), st.getDescription());
+			listeStatus.add(item);
+		}
+		return listeStatus;
+	}
+
+	public void setListeStatus(ArrayList<SelectItem> listeStatus) {
+		this.listeStatus = listeStatus;
+	}
+
+	public void setListeHobbies(ArrayList<SelectItem> listeHobbies) {
+		this.listeHobbies = listeHobbies;
+	}
+
+	public ArrayList<SelectItem> getListeHobbies() {
+
+		listeHobbies = new ArrayList<SelectItem>();
+		ArrayList<Hobby> hobbies = new ArrayList<Hobby>();
+		hobbies = HobbyManager.getListeHobbies();
+		for (Hobby h : hobbies) {
+			SelectItem item = new SelectItem(h.getId(), h.getDescription());
+			listeHobbies.add(item);
+		}
+
+		return listeHobbies;
+	}
+
+	public ArrayList<SelectItem> getListeVilles() {
+		listeVilles = new ArrayList<SelectItem>();
+		ArrayList<City> cities = new ArrayList<City>();
+		cities = CityManager.getListeCities();
+		for (City c : cities) {
+			SelectItem item = new SelectItem(c.getId(), c.getDescription());
+			listeVilles.add(item);
+		}
+		return listeVilles;
+	}
+
+	public void setListeVilles(ArrayList<SelectItem> listeVilles) {
+		this.listeVilles = listeVilles;
+	}
+
+	public String rechercher()
+	{
+		String redirection=null;
+		HashMap<String, Object> criteresRecherche=new HashMap<String, Object>();
+		
+		criteresRecherche.put("categorie_id", categorie_id);
+		criteresRecherche.put("hairColor_id", hairColor_id);	
+		criteresRecherche.put("skinColor_id", skinColor_id);
+		criteresRecherche.put("eyeColor_id", eyeColor_id);
+		criteresRecherche.put("typePhysique_id", typePhysique_id);
+		criteresRecherche.put("ville_id", ville_id);
+		criteresRecherche.put("status_id", status_id);
+		criteresRecherche.put("hobbies_id", hobbies_id);
+		criteresRecherche.put("tailleMax", tailleMax);
+		criteresRecherche.put("tailleMin", tailleMin);
+		criteresRecherche.put("fumeur", fumeur);
+		
+		listeResultat=MembreManager.recherche(criteresRecherche);
+		if(!listeResultat.isEmpty()) //si resultats trouves
+			redirection="oui";
+		else
+			redirection="non";
+		return redirection;
+	}
+	
+	public String ajouterMembre(Membre membre)
+	{
+		String destination=null;
+		//tester niveau du user
+		NiveauMembre niveau=new NiveauMembre();
+		int maxFavoris=0;
+		niveau=mbb.getMembre().getNiveauMembre();
+		maxFavoris=niveau.getNbFavorisMax();
+		
+		ArrayList<Membre> listeFavoris=new ArrayList<Membre>();
+		listeFavoris=mbb.getListeFavoris();
+		
+		if(listeFavoris.size()<maxFavoris){
+			//ajouter amis
+			int ajoute=MembreManager.ajouterFavoris(mbb.getMembre(), membre);
+			
+			if(ajoute>0){
+				//envoyer courriel a qui a ete ajoute
+				SendEmail.envoyerMail(mbb.getMembre(), membre, "ajoute");
+			}
+		}
+		return destination;
+	}
+	
+	public String envoyerClinDoeil(Membre membre)
+	{
+		String destination=null;
+		int envoye=0;
+		envoye=ClinsdoeilManager.envoyerClinDoeil(mbb.getMembre(), membre);
+		
+		return destination;
+	}
+	
+}
